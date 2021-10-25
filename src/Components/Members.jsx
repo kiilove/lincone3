1147f7d02ebcf9d92d84c87a8c0fa842417119af
kiilove <jbkim@jncore.com>
2021-10-25 14:28:57 +0900

    commit on "main"

--- src/Components/Members.jsx
+++ src/Components/Members.jsx
@@ -1,6 +1,7 @@
 import { Button, Avatar } from "@mui/material";
-import { Grade, Mail, Person, Phone } from "@material-ui/icons";
 import styled from "styled-components";
+
+import { memData } from "../tempdata/member";
 
 const Container = styled.div`
   width: 100%;
@@ -10,10 +11,13 @@ const Container = styled.div`
 const Wrapper = styled.div`
   display: flex;
   flex-wrap: wrap;
+  width: 100%;
+  justify-content: space-around;
 `;
 
 const CardContainer = styled.div`
   display: flex;
+  justify-content: center;
   width: 190px;
   height: 250px;
   background-color: white;
@@ -21,32 +25,77 @@ const CardContainer = styled.div`
   border: 1px solid #f7f0e9;
   -webkit-box-shadow: 1px 2px 3px -3px #d8dacf;
   box-shadow: 1px 2px 3px -3px #d8dacf;
-  margin-right: 15px;
   margin-bottom: 20px;
+`;
+
+const CardWrapper = styled.div`
+  width: 90%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const CardAvatar = styled.div`
   width: 100%;
   height: 120px;
-  background-color: indigo;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const CardInfo = styled.div`
+  width: 100%;
+  margin-top: 20px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const UserName = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+`;
+
+const UserEmail = styled.span`
+  font-size: 13px;
+  font-weight: 100;
+`;
+
+const CardAction = styled.div`
+  margin-top: 20px;
 `;
 
 const Members = () => {
+  const CardDom = ({ item }) => {
+    return (
+      <CardWrapper key={item.id}>
+        <CardAvatar>
+          <Avatar sx={{ width: 100, height: 100 }} />
+        </CardAvatar>
+        <CardInfo>
+          <UserName>{item.userName}</UserName>
+          <UserEmail>{item.userEmail}</UserEmail>
+        </CardInfo>
+        <CardAction>
+          <Button variant="outlined" color="success">
+            상세보기
+          </Button>
+        </CardAction>
+      </CardWrapper>
+    );
+  };
+
   return (
     <Container>
       <Wrapper>
-        <CardContainer>
-          <CardAvatar>
-            <Avatar sx={{ width: 100, height: 100 }} />
-          </CardAvatar>
-        </CardContainer>
-        <CardContainer></CardContainer>
-        <CardContainer></CardContainer>
-        <CardContainer></CardContainer>
-        <CardContainer></CardContainer>
-        <CardContainer></CardContainer>
-        <CardContainer></CardContainer>
-        <CardContainer></CardContainer>
+        {memData.map((item) => (
+          <CardContainer>
+            <CardDom item={item} />
+          </CardContainer>
+        ))}
       </Wrapper>
     </Container>
   );
---
+++ src/tempdata/member.js
@@ -0,0 +1,37 @@
+export const memData = [
+  {
+    id: 1,
+    userName: "김진배",
+    userEmail: "jbkim@jncore.com",
+  },
+  {
+    id: 2,
+    userName: "고윤주",
+    userEmail: "laroux@jncore.com",
+  },
+  {
+    id: 3,
+    userName: "유한준",
+    userEmail: "willok@jncore.com",
+  },
+  {
+    id: 4,
+    userName: "최헌정",
+    userEmail: "gran@jncore.com",
+  },
+  {
+    id: 5,
+    userName: "조유나",
+    userEmail: "yuna@jncore.com",
+  },
+  {
+    id: 6,
+    userName: "고윤희",
+    userEmail: "sarang@jncore.com",
+  },
+  {
+    id: 6,
+    userName: "세금계산서",
+    userEmail: "etax@jncore.com",
+  },
+];
